c0e673ab75bf1adfd44be00fd322b809ad7b1f50
JengSiang <jengsiang13@gmail.com>
2025-05-02 15:48:08 +0800

    Complete Supplier Management

---
+++ src/SupplierManagement/SupplierOperations.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package SupplierManagement;
+
+/**
+ *
+ * @author Jengsiang
+ */
+import java.util.*;
+public interface SupplierOperations {
+    void add();
+    void clear();
+    void update();
+    void delete();
+    void refresh();
+}
