42ac011232cb60636059128027e61214a1a4b72c
notedwin-dev <notedwin.music@gmail.com>
2025-05-15 12:02:19 +0800

    Refactor Main class to extend javax.swing.JFrame for GUI integration

--- src/itemmanagement/Main.java
+++ src/itemmanagement/Main.java
@@ -8,7 +8,7 @@ package itemmanagement;
  *
  * @author nixon
  */
-public class Main {
+public class Main extends javax.swing.JFrame {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
